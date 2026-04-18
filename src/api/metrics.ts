663c6253b123ed9183f57a4f1baa51d2fa4e95b1
Edyth-K <kelvin.dcruz@gmail.com>
2026-04-18 15:21:57 -0400

    Add admin namespace with HTML metrics page

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -2,7 +2,11 @@ import { Request, Response } from "express";
 import { apiConfig } from "../config.js";
 
 export function handlerMetrics(req: Request, res: Response) {
-    const currentHitCount = apiConfig.fileserverHits;
-    res.set("Content-Type", "text/plain; charset=utf-8");
-    res.send(`Hits: ${currentHitCount}`);
+    res.set("Content-Type", "text/html; charset=utf-8");
+    res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${apiConfig.fileserverHits} times!</p>
+  </body>
+</html>`);
 }
