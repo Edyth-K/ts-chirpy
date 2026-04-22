19b3c6df79b80c26bfc41d6a61fc1c664c44ca71
Edyth-K <kelvin.dcruz@gmail.com>
2026-04-21 22:57:03 -0500

    Change /admin/reset to POST method

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const PORT = 8080;
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static('./src/app'));
 app.get("/admin/metrics", handlerMetrics);
-app.get("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerReset);
 app.get("/api/healthz", handlerReadiness);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
